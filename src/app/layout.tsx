03bfd475d95c46c37ec916640fecbcf2ce140818
ttahm3d <ttahm3d@gmail.com>
2024-07-21 11:39:12 +0530

    [feat] - completed UI design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={fonts.className}>
+      <body className={`${fonts.className} flex-col flex`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
--- src/app/snapshot/page.tsx
+++ src/app/snapshot/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { Slider } from "@/components/ui/slider";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { Input } from "@/components/ui/input";
 
 import {
   Select,
@@ -14,32 +16,32 @@ import { useState } from "react";
 export default function SnapshotPage() {
   const [text, setText] = useState<string>("");
   const [outputLength, setOutputLength] = useState<string>("400");
-  const [numberOfPoints, setNumberOfPoints] = useState<number[]>([3]);
+  const [numberOfPoints, setNumberOfPoints] = useState<number | undefined>(3);
 
-  console.log(text, outputLength, numberOfPoints);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.value;
+    const parsedValue = newValue === "" ? undefined : parseFloat(newValue);
+    setNumberOfPoints(parsedValue);
+  };
+
   return (
-    <div className="container grid grid-cols-1 ">
-      <div className="pt-8 px-4">
-        <div className="flex flex-col gap-1">
-          <label
-            htmlFor="input-text"
-            className="mb-2 text-sm font-medium text-muted-foreground">
-            Text
-          </label>
-          <textarea
+    <div className="flex-1 container grid grid-cols-3 place-items-stretch h-full content-center min-h-full">
+      <div className="py-8 px-4 col-span-1 grid grid-cols-2 gap-8">
+        <div className="flex flex-col gap-2 col-span-2">
+          <Label htmlFor="input-text">Text</Label>
+          <Textarea
             value={text}
             onChange={(e) => setText(e.target.value)}
             id="input-text"
-            className="w-full resize-none py-2 px-3 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 rounded-md border border-input bg-background"
+            className="w-full resize-y py-2 px-3 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 rounded-md border border-input bg-background"
             rows={3}
-            placeholder="Enter the text to be summarized"></textarea>
+            placeholder="Enter the text to be summarized"></Textarea>
+          <small className="text-neutral-500 text-xs">
+            {text.length} characters
+          </small>
         </div>
-        <div className="flex flex-col gap-1 mt-4">
-          <label
-            htmlFor="output-length"
-            className="mb-2 text-sm font-medium text-muted-foreground">
-            Output Length
-          </label>
+        <div className="flex flex-col col-span-1 flex-1 gap-2 mt-4">
+          <Label htmlFor="output-length">Output Length</Label>
           <Select onValueChange={setOutputLength} defaultValue={outputLength}>
             <SelectTrigger>
               <SelectValue placeholder="Select a verified email to display" />
@@ -53,25 +55,27 @@ export default function SnapshotPage() {
             </SelectContent>
           </Select>
         </div>
-        <div className="flex flex-col gap-1 mt-4">
-          <label
-            htmlFor="number-of-points"
-            className="mb-2 text-sm font-medium text-muted-foreground">
-            Output Length
-          </label>
-          <Slider
-            id="number-of-points"
-            value={numberOfPoints}
-            onValueChange={(value) => setNumberOfPoints(value)}
-            defaultValue={[3]}
+        <div className="flex flex-1 flex-col col-span-1 self-center gap-2 mt-4">
+          <Label htmlFor="bullet-points">Bullet Points</Label>
+          <Input
+            id="number-input"
+            type="number"
+            value={numberOfPoints === undefined ? "" : numberOfPoints}
+            onChange={handleChange}
+            min={2}
             max={7}
             step={1}
           />
+          <small className="text-neutral-500 text-xs">
+            7 bullet points at max
+          </small>
         </div>
-        <Button className="mt-4">Generate Snapshot</Button>
+        <Button className="mt-4 col-span-2" disabled={text === ""}>
+          Generate Snapshot
+        </Button>
       </div>
-      <div className="pt-8 px-4">
-        <div className="border-dotted border p-4 h-full bg-primary text-primary-foreground">
+      <div className="py-8 px-4 col-span-2">
+        <div className="border-dotted border p-4 h-full text-primary bg-secondary">
           Please provide the details to see the output
         </div>
       </div>
